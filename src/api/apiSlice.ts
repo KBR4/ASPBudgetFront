ad49771ac28136a37fb9085c26a86d978e08e71b
SokolovK <turnshluhe@gmail.com>
2025-06-12 14:15:48 +0300

    Fixed login issues

--- src/api/apiSlice.ts
+++ src/api/apiSlice.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: process.env.REACT_APP_API_URL ?? 'http://localhost:5259',
+  baseUrl: process.env.REACT_APP_API_URL ?? 'https://localhost:7032',
   credentials: 'include',
   prepareHeaders(headers) {
     return headers;
